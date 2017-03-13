8f4915549a7e9c2ddf7bf842b7423a4d1dacea61
David Piper <dbpiper@gmail.com>
2017-03-12 19:52:50 -0500

    Add clamping to xRotation. Finish video 2 of tutorial.

--- FPS/Assets/MouseLookScript.js
+++ FPS/Assets/MouseLookScript.js
@@ -1,13 +1,19 @@
 ﻿#pragma strict
 
 var lookSensitivity : float = 5;
-var yRotation : float;
-var xRotation : float;
-var currentYRotation : float;
-var currentXRotation : float;
-var yRotationV : float;
-var xRotationV : float;
 var lookSmoothDamp : float = 0.1;
+@HideInInspector
+var yRotation : float;
+@HideInInspector
+var xRotation : float;
+@HideInInspector
+var currentYRotation : float;
+@HideInInspector
+var currentXRotation : float;
+@HideInInspector
+var yRotationV : float;
+@HideInInspector
+var xRotationV : float;
 
 function Start () {
 	
@@ -16,6 +22,8 @@ function Start () {
 function Update () {
     yRotation += Input.GetAxis("Mouse X") * lookSensitivity;	
     xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+    xRotation = Mathf.Clamp(xRotation, -90, 90);
 
     currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, xRotationV, lookSmoothDamp);
     currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, yRotationV, lookSmoothDamp);
