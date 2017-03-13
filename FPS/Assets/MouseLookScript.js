3f86aae1660f2c62c341b27a287e45b237ebf265
David Piper <dbpiper@gmail.com>
2017-03-12 19:43:33 -0500

    Add mouse control of camera.

---
+++ FPS/Assets/MouseLookScript.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var lookSensitivity : float = 5;
+var yRotation : float;
+var xRotation : float;
+var currentYRotation : float;
+var currentXRotation : float;
+var yRotationV : float;
+var xRotationV : float;
+var lookSmoothDamp : float = 0.1;
+
+function Start () {
+	
+}
+
+function Update () {
+    yRotation += Input.GetAxis("Mouse X") * lookSensitivity;	
+    xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+    currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, xRotationV, lookSmoothDamp);
+    currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, yRotationV, lookSmoothDamp);
+
+    transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+}
