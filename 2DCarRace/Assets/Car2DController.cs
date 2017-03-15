c4d49102b3bf396eebb3d88fda47c5697ced013b
David Piper <dbpiper@gmail.com>
2017-03-15 18:25:41 -0500

    Get the basic car driving working. Up to about 40 min in the video tutorial.

--- 2DCarRace/Assets/Car2DController.cs
+++ 2DCarRace/Assets/Car2DController.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Car2DController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    float speedForce = 10f;
+    float torqueForce = -200f;
+    float driftFactor = 0.999f;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = ForwardVelocity() + RightVelocity() * driftFactor;
+        if (Input.GetButton("Accelerate")) {
+            rb.AddForce(transform.up * speedForce);
+        }
+        rb.angularVelocity = Input.GetAxis("Horizontal") * torqueForce;
+
 	}
+
+    Vector2 ForwardVelocity() {
+        return transform.up * Vector2.Dot(
+            GetComponent<Rigidbody2D>().velocity,
+            transform.up
+        ); 
+    }
+
+    Vector2 RightVelocity() {
+        return transform.right * Vector2.Dot(
+            GetComponent<Rigidbody2D>().velocity,
+            transform.right
+        );
+    }
 }
