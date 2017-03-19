67f9030b70d574b0376347545d756a4773c74381
David Piper <dbpiper@gmail.com>
2017-03-19 10:42:57 -0500

    Change animation to use blend trees. Make camera follow player.

--- 2DRpg/Assets/Scripts/PlayerController.cs
+++ 2DRpg/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
 
     [SerializeField]
     private float moveSpeed;
@@ -12,23 +14,32 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        lastMove = Vector2.zero;
 	}
 
     public void FixedUpdate() {
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
-        if (xAxis != 0f) {
+        playerMoving = false;
+        if (xAxis > 0.1f || xAxis < -0.1f) {
             transform.Translate(new Vector3(
                xAxis * moveSpeed, 0, 0 
             ));
+            playerMoving = true;
+            lastMove = new Vector2(xAxis, 0f);
         }
-        if (yAxis != 0f) {
+        if (yAxis > 0.1f || yAxis < -0.1f) {
             transform.Translate(new Vector3(
                0, yAxis * moveSpeed, 0 
             ));
+            playerMoving = true;
+            lastMove = new Vector2(0f, yAxis);
         }
         anim.SetFloat("MoveX", xAxis);
         anim.SetFloat("MoveY", yAxis);
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 
     // Update is called once per frame
