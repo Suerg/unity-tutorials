53a9f9f77671d36680a82d9e8f0666c15d6ab530
David Piper <dbpiper@gmail.com>
2017-03-18 22:47:11 -0500

    Create player walk animation.

---
+++ 2DRpg/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator anim;
+
+    [SerializeField]
+    private float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    public void FixedUpdate() {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        if (xAxis != 0f) {
+            transform.Translate(new Vector3(
+               xAxis * moveSpeed, 0, 0 
+            ));
+        }
+        if (yAxis != 0f) {
+            transform.Translate(new Vector3(
+               0, yAxis * moveSpeed, 0 
+            ));
+        }
+        anim.SetFloat("MoveX", xAxis);
+        anim.SetFloat("MoveY", yAxis);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
